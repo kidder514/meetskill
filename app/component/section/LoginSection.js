d00b1c024bc15a13908e05b3e3392a472bc6ee3e
Duo Lu <kidder5142@gmail.com>
2017-11-07 22:36:33 +1100

    google login button implemented without any backend integration

--- app/component/section/LoginSection.js
+++ app/component/section/LoginSection.js
@@ -1,28 +1,51 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import string from "../../String";
+import config from "../../config"
+import GoogleLogin from 'react-google-login';
 
-class LoginSection extends Component{
-	constructor(props){
+class LoginSection extends Component {
+	constructor(props) {
 		super(props);
 		this.state = {
-			email : '',
+			email: '',
 			errorEmail: '',
 			password: '',
 			errorPassword: ''
 		};
-		this.submit = this.submit.bind(this);		
+		this.submit = this.submit.bind(this);
+		this.gLoginSuccess = this.gLoginSuccess.bind(this);
 	}
 
-	submit(){
+	gLoginSuccess(googleUser){
+		if (googleUser !== undefined && googleUser.getAuthResponse().id_token !== ""){
+			console.log(googleUser);
+			console.log(googleUser.getAuthResponse());
+			// this.props.gValidationCall(googleUser.getAuthResponse().id_token);
+			// this.props.login(googleUser.getAuthResponse().id_token, this.state.rememberLogin);
+		}
+	}
+	
+	gLoginFail(){
+		// login fail
+	}
+
+	gSignout(){
+	    var auth2 = gapi.auth2.getAuthInstance();
+	    auth2.signOut().then(function () {
+	    	// google signout callback
+	    });
+	}
+
+	submit() {
 		var query = "";
 		this.props.loginCall(query);
 	}
 
-    render(){
-	    return (
-	    	<div className="login-section">
+	render() {
+		return (
+			<div className="login-section">
 				<div className="input-item">
-	    			<input type="text" name="email" placeholder={string.email} />
+					<input type="text" name="email" placeholder={string.email} />
 					<span></span>
 				</div>
 				<div className="input-item">
@@ -35,14 +58,19 @@ class LoginSection extends Component{
 				</div>
 				<hr />
 				<div className="input-item">
-					<button type="button">{string.LoginWithGoogle}</button>
+					<GoogleLogin
+						    clientId={config.googleLoginClientId}
+						    buttonText={string.LoginWithGoogle}
+						    onSuccess={this.gLoginSuccess}
+						    onFailure={this.gLoginFail}
+						/>
 				</div>
 				<div className="input-item">
 					<button type="button">{string.LoginWithFacebook}</button>
 				</div>
-	    	</div>
-	    )
-  	}
+			</div>
+		)
+	}
 }
 
 export default LoginSection;
--- app/config.js
+++ app/config.js
@@ -1,6 +1,7 @@
 const config = {
 	logoUrl:"http://downloadicons.net/sites/default/files/round-icon-68041.png",
 	APIserver:"http://www.mocky.io/v2/596714a3110000cd0db6c077",
+	googleLoginClientId:"1044894519430-hv76r463h1oec25f05r8i530djhjp92m.apps.googleusercontent.com",
 	colorIce:"#99D3Df",
 	colorWater:"#88BBD6",
 	colorPlaster:"#CDCDCD",
